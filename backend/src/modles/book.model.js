e96aeb3427c9e85e102cfe553e1120420e958b1c
Karma Deki Lhazin <tomiedelrey762@gmail.com>
2025-06-10 16:19:50 -0700

    made models

--- backend/src/modles/book.model.js
+++ backend/src/modles/book.model.js
@@ -1,1 +1,16 @@
 //book model
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: {
+    type: String,
+    enum: ["Science Fiction", "Mystery", "Fantasy", "Romance"],
+    required: true,
+  },
+  year: { type: Number, required: true },
+});
+
+const Book = mongoose.model("Book", BookSchema);
+module.exports = Book;
