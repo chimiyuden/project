ae31c45b364d14253f4ca7fe08ed7e7776c128cb
Your Name <tsheringphuntsho1200@gmail.com>
2025-06-10 12:56:00 +0600

    made middleware

--- backend/src/middleware/verifyAuth.middlerware.js
+++ backend/src/middleware/verifyAuth.middlerware.js
@@ -1,1 +1,20 @@
-// verify auth
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key';
+
+function verifyToken(req, res, next) {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access Denied: No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid Token' });
+  }
+}
+
+module.exports = verifyToken;
