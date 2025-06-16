33bb94624ef48da43e4a0b611239a61434c735d7
Chimi <chimiyuden19@gmail.com>
2025-06-16 14:35:44 +0600

    made register and home

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,1 +1,25 @@
-//for home
+const initialData = {
+  title: "",
+  author: "",
+  genre: "Science Fiction",
+  year: new Date().getFullYear(),
+};
+
+const Home = () => {
+  const handelDialog = () => {
+    setDialogOpen(isOpen);
+    if (!isOpen) {
+      setForm({ ...initialData });
+    }
+  };
+  return (
+    <div>
+      <h1>Book Collection</h1>
+      <button className="add-button" onClick={() => handelDialog(true)}>
+        Add Book
+      </button>
+    </div>
+  );
+};
+
+export default Home;
