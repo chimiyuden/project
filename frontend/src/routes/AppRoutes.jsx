d30743806d9781c0fcdcdad53a6e46799734dae9
Your Name <tsheringphuntsho1200@gmail.com>
2025-06-18 10:19:28 +0600

    added protectedroutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,19 @@ import Home from "../pages";
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import Profile from "../pages/auth/Profile";
+import Loading from "../components/Loading";
+
+const ProtectedRoutes = ({ children }) => {
+  const { isLoggedIn, isLoading } = useAuth();
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!isLoading && isLoggedIn) {
+    return children;
+  }
+  return <Navigate to={"/login"} />;
+};
 
 const AppRoutes = () => {
   return (
