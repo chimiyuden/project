f86640ec1d7833de89cdc5048b5ecbf1bb866acf
Chimi <chimiyuden19@gmail.com>
2025-06-23 16:39:49 +0600

    made some changes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/data")
+    fetch("http://localhost:3000")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
